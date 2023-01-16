54f3bd437f88e8a96a3ddf2d11b2bb5782b20893
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-01-16 15:50:12 -0500

    JSX

--- firstReact/index.js
+++ firstReact/index.js
@@ -1,17 +1,51 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+//<h1 class="header">Hello world</h1>
 
-/* 
-Challenge - recreate the above line of code in vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
+// const element = <h1 className = "header"> This is JSX</h1>
+// console.log(element)
 
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-    
-*/
+/*
+{
+    type: "h1", 
+    key: null, 
+    ref: null, 
+    props: {className: "header", children: "This is JSX"},
+    _owner: null, 
+    _store: {}
+}
+ */
 
-const h1 = document.createElement("h1")
-h1.textContent = "This is an imperative way to program"
-h1.className = "header"
-document.getElementsById("root").append(h1)
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+// //JSX
+// ReactDOM.render(
+//     page, 
+//     document.getElementById("root")
+//     )
+
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro </h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+)
+
