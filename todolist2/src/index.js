6d1b2b7bf644b99ac7fe1b1856fa560d1603db59
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-01-23 22:16:49 -0500

    css transition

--- todolist2/src/index.js
+++ todolist2/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoList from './TodoList';
+import App from './App';
+// import TodoList from './TodoList';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoList />
+    {/* <TodoList /> */}
+    <App />
   </React.StrictMode>
 );
 
