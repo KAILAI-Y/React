0e8ed63cd9673da8918ebc367022fdd6e20d37f7
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-01-30 22:55:25 -0500

    action Creators

--- todolist3/src/TodoList.js
+++ todolist3/src/TodoList.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import { Input, Button, List } from 'antd';
 import store from './store/index.js';
-import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './store/actionType';
+import { getAddItemAction, getDeleteItemAction, getInputChangeAction } from './store/actionCreators';
 
 class TodoList extends Component{
 
@@ -39,10 +39,7 @@ class TodoList extends Component{
     }
 
     handleInputChange(e){
-        const action = {
-            type: CHANGE_INPUT_VALUE,
-            value: e.target.value
-        }
+        const action = getInputChangeAction(e.target.value);
         store.dispatch(action);
     }
 
@@ -51,18 +48,13 @@ class TodoList extends Component{
     }
 
     handleBtnClick(){
-        const action = {
-            type: ADD_TODO_ITEM
-        }
+        const action = getAddItemAction();
         store.dispatch(action);
     }
 
     handleItemDelete(index){
-       const action = {
-        type: DELETE_TODO_ITEM,
-        index
-       }
-       store.dispatch(action);
+        const action = getDeleteItemAction(index);
+        store.dispatch(action);
 
     }
 }
