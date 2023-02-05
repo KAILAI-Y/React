248163260a742a9b28a3343b0e6ea89aa0407060
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-02-04 23:57:41 -0500

    react_redux

--- todolist3/src/TodoList.js
+++ todolist3/src/TodoList.js
@@ -1,58 +1,44 @@
-import React, {Component} from "react";
-import store from './store/index.js';
-import { getAddItemAction, getDeleteItemAction, getInputChangeAction, getInitList} from './store/actionCreators';
-import TodoListUI from "./TodoListUI.js";
+import React, { Component } from "react";
+import { connect} from "react-redux";
 
 class TodoList extends Component{
 
-    constructor(props){
-        super(props);
-        this.state = store.getState();
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleStoreChange = this.handleStoreChange.bind(this);
-        this.handleBtnClick = this.handleBtnClick.bind(this);
-        this.handleItemDelete = this.handleItemDelete.bind(this);
-        store.subscribe(this.handleStoreChange);
-    }
-
-    render(){
-        return (
-            <TodoListUI 
-              inputValue={this.state.inputValue}
-              list={this.state.list}
-              handleInputChange={this.handleInputChange}
-              handleBtnClick={this.handleBtnClick}
-              handleItemDelete={this.handleItemDelete}
-            />
-            
-        )
-    }
-
-    componentDidMount(){
-        const action = getInitList();
-        // console.log(action);
-        store.dispatch(action);
-    }
-
-    handleInputChange(e){
-        const action = getInputChangeAction(e.target.value);
-        store.dispatch(action);
-    }
-
-    handleStoreChange(){
-        this.setState(store.getState());
-    }
-
-    handleBtnClick(){
-        const action = getAddItemAction();
-        store.dispatch(action);
-    }
-
-    handleItemDelete(index){
-        const action = getDeleteItemAction(index);
-        store.dispatch(action);
-
-    }
+  render(){
+    return (
+      <div>
+        <div>
+          <input value={this.props.inputValue} onChange={this.props.changeInputValue}/>
+          <button>ADD</button>
+        </div>
+        <ul>
+          <li>english</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default TodoList;
+const mapStateToProps = (state) => {
+  return{
+    inputValue: state.inputValue
+  }
+}
+
+
+//store.dispatch, props
+const mapDispatchToProps = (dispatch) => {
+  return{
+    changeInputValue(e){
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action);
+    }
+
+
+  }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- todolist3/src/index.js
+++ todolist3/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoList from './TodoList';
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <TodoList />
-  </React.StrictMode>
+  App
 );
 
