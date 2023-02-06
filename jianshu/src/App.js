07569af39dd24ffacfdd60deca484b841b9f2cbd
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-02-06 11:10:04 -0500

    jianshu

---
+++ jianshu/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+import { GlobalStyle } from "./style";
+
+class App extends Component{
+  render(){
+    return (
+      <Fragment>
+        <div className="dell">
+          hello world
+        </div>
+        <GlobalStyle />
+      </Fragment>
+    );
+
+  }
+}
+
+export default App;
