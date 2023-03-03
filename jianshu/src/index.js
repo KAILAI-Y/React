96ae4cb293bb529a7a0f5d9fc6c7440eda81010a
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-03-02 23:37:59 -0500

    nav header

--- jianshu/src/index.js
+++ jianshu/src/index.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { GlobalStyle } from "./style";
 import App from './App';
 import './style.js';
 
 ReactDOM.render(
     <Fragment>
-      <GlobalStyle />
       <App />
     </Fragment>,
     document.getElementById('root')
