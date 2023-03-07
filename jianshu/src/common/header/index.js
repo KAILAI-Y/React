767b0ccbeb261c767837dafca5cdcea3ed90df5d
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-03-07 16:16:51 -0500

    actionCreators & constants

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { CSSTransition } from "react-transition-group";
+import { actionCreators } from "./store";
 import {
     HeaderWrapper,
     Logo,
@@ -61,16 +62,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     handleInputFocus(){
-      const action = {
-        type: 'search_focus'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchFocus());
     },
     handleInputBlur(){
-      const action = {
-        type: 'search_blur'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchBlur());
     }
   }
 }
---
+++ jianshu/src/common/header/store/constants.js
@@ -0,0 +1,2 @@
+export const SEARCH_FOCUS = 'header/SAERCH_FOCUS';
+export const SEARCH_BLUR = 'header/SAERCH_BLUR';
