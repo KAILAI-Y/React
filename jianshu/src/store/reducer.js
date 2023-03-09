2a75cf48c07a3c2d0f969a8f20df8c67b8bd31f6
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-03-09 16:13:28 -0500

    redux-immutable

--- jianshu/src/store/reducer.js
+++ jianshu/src/store/reducer.js
@@ -1,5 +1,6 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux-immutable';
 import { reducer as headReducer} from '../common/header/store';
+
 
 const reducer = combineReducers({
     header: headReducer
