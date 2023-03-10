c314dd6c06d07150531a915dfb7f4f8d1e8b8891
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-03-10 15:02:16 -0500

    热门搜素样式布局

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -10,8 +10,39 @@ import {
     NavSearch,
     Addition,
     Button,
-    SearchWrapper
+    SearchWrapper, 
+    SearchInfo, 
+    SearchInfoTitle,
+    SearchInfoSwitch,
+    SearchInfoItem,
+    SearchInfoList
 } from './style';
+
+const getListArea = (show) => {
+  if(show){
+    return (
+      <SearchInfo>
+        <SearchInfoTitle>热门搜索
+          <SearchInfoSwitch>
+            换一批
+          </SearchInfoSwitch>
+        </SearchInfoTitle>
+        <SearchInfoList>
+          <SearchInfoItem>教育</SearchInfoItem>
+          <SearchInfoItem>教育</SearchInfoItem>
+          <SearchInfoItem>教育</SearchInfoItem>
+          <SearchInfoItem>教育</SearchInfoItem>
+          <SearchInfoItem>教育</SearchInfoItem>
+          <SearchInfoItem>教育</SearchInfoItem>
+        </SearchInfoList>
+      </SearchInfo>
+
+    )
+  }else{
+    return null;
+  }
+
+}
 
 const Header = (props) => {
   return(
@@ -38,6 +69,7 @@ const Header = (props) => {
                 </NavSearch>
               </CSSTransition>
               <i className={props.focused ? ' focused iconfont': 'iconfont'}>&#xe623;</i>
+              {getListArea(props.focused)}
           </SearchWrapper>   
       </Nav>
       <Addition>
