13bf1fed34ffb7184430ecfa46671d522713e1ca
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-03-12 12:24:57 -0400

    热门搜索换页功能实现

--- jianshu/src/common/header/store/constants.js
+++ jianshu/src/common/header/store/constants.js
@@ -1,3 +1,6 @@
 export const SEARCH_FOCUS = 'header/SAERCH_FOCUS';
 export const SEARCH_BLUR = 'header/SAERCH_BLUR';
 export const CHANGE_LIST = 'heder/CHANGE_LIST';
+export const MOUSE_ENTER = 'heder/MOUSE_ENTER';
+export const MOUSE_LEAVE = 'heder/MOUSE_LEAVE';
+export const CHANGE_PAGE = 'heder/CHANGE_PAGE';
