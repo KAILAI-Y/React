b019773f31254991acac80b06ec94c2c295d452f
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-10-04 15:52:49 -0400

    props-object

--- 01-fundamental/my-app/src/index.js
+++ 01-fundamental/my-app/src/index.js
@@ -2,50 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const firstBook = {
-  author: ' Michael Lewis',
-  title: 'Going Infinite: The Rise and Fall of a New Tycoon',
-  img: 'https://m.media-amazon.com/images/I/81LUGiJk5iL._SL1500_.jpg',
-}
+const books = [
+  {
+    author: ' Michael Lewis',
+    title: 'Going Infinite: The Rise and Fall of a New Tycoon',
+    img: 'https://m.media-amazon.com/images/I/81LUGiJk5iL._SL1500_.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    img: 'https://m.media-amazon.com/images/I/81bGKUa1e0L._SL1500_.jpg',
+    id: 2,
+  },
+]
 
-const secondBook = {
-  author: 'James Clear',
-  title:
-    'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-  img: 'https://m.media-amazon.com/images/I/81bGKUa1e0L._SL1500_.jpg',
-}
-
-const BookList = () => {
+function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
-        </p>
-        <button>click me</button>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props
+  const { img, title, author } = props.book
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
