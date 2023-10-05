ef420f9e2deab5ecc9fbc13cc0e9ee24ac6043f2
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-10-04 21:34:15 -0400

    modules

---
+++ 01-fundamental/my-app/src/books.js
@@ -0,0 +1,26 @@
+import img1 from './img/img1.jpg'
+import img2 from './img/img2.jpg'
+import img3 from './img/img3.jpg'
+
+const books = [
+  {
+    author: 'Michael Lewis',
+    title: 'Going Infinite: The Rise and Fall of a New Tycoon',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Walter Isaacson',
+    title: 'Elon Musk',
+    img: img3,
+    id: 3,
+  },
+]
+
+export default books
--- 01-fundamental/my-app/src/index.js
+++ 01-fundamental/my-app/src/index.js
@@ -1,74 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import books from './books'
 
-const books = [
-  {
-    author: ' Michael Lewis',
-    title: 'Going Infinite: The Rise and Fall of a New Tycoon',
-    img: 'https://m.media-amazon.com/images/I/81LUGiJk5iL._SL1500_.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title:
-      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-    img: 'https://m.media-amazon.com/images/I/81bGKUa1e0L._SL1500_.jpg',
-    id: 2,
-  },
-]
-
-function BookList() {
+const BookList = () => {
   return (
-    <section className="booklist">
-      <EventExamples />
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />
-      })}
-    </section>
-  )
-}
-
-const EventExamples = () => {
-  const handleFormInput = (e) => {
-    console.log(e)
-    console.log(`Input Name : ${e.target.name}`)
-    console.log(`Input Name : ${e.target.value}`)
-  }
-
-  const handleButtonClick = () => {
-    alert('handle button click')
-  }
-
-  const handleFormSubmission = (e) => {
-    e.preventDefault()
-    console.log('form submitted')
-  }
-
-  return (
-    <section>
-      <form onSubmit={handleFormSubmission}>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: '1rem 0' }}
-        />
-        <button type="submit">submit form</button>
-      </form>
-      <button onClick={handleButtonClick}>click me</button>
-    </section>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />
+        })}
+      </section>
+    </>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author } = props
+  const { img, title, author, number } = props
+
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <span className="number">{`# ${number + 1}`}</span>
     </article>
   )
 }
