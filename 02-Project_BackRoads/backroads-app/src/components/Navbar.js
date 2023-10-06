b0ea191b68cd0b835edbed5da0425a3c0347c21f
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-10-06 00:00:09 -0400

    navbar css

--- 02-Project_BackRoads/backroads-app/src/components/Navbar.js
+++ 02-Project_BackRoads/backroads-app/src/components/Navbar.js
@@ -1,22 +1,23 @@
 import logo from '../images/logo.svg'
 import { pageLinks, socialLinks } from '../data'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src={logo} className="nav-logo" alt="backroads" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
+    <nav className={styles.navbar}>
+      <div className={styles.nav_center}>
+        <div className={styles.nav_header}>
+          <img src={logo} className={styles.nav_logo} alt="backroads" />
+          {/* <button type="button" className={styles.nav_toggle} id="nav-toggle">
             <i className="fas fa-bars"></i>
-          </button>
+          </button> */}
         </div>
         {/* <!-- left this comment on purpose --> */}
-        <ul className="nav-links" id="nav-links">
+        <ul className={styles.nav_links} id="nav-links">
           {pageLinks.map((link) => {
             return (
               <li key={link.id}>
-                <a href={link.href} className="nav-link">
+                <a href={link.href} className={styles.nav_link}>
                   {link.text}
                 </a>
               </li>
@@ -24,7 +25,7 @@ const Navbar = () => {
           })}
         </ul>
 
-        <ul className="nav-icons">
+        <ul className={styles.nav_icons}>
           {socialLinks.map((link) => {
             const { id, href, icon } = link
             return (
@@ -33,7 +34,7 @@ const Navbar = () => {
                   href={href}
                   target="_blank"
                   rel="noreferrer"
-                  className="nav-icon"
+                  className={styles.nav_icon}
                 >
                   <i className={icon}></i>
                 </a>
