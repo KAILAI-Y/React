d4c4fb9a63ec3287f7db674f3e6fb14c1289f0da
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-10-06 12:55:57 -0400

    component - PageLinks

--- 02-Project_BackRoads/backroads-app/src/components/Footer.js
+++ 02-Project_BackRoads/backroads-app/src/components/Footer.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { pageLinks, socialLinks } from '../data'
+import { socialLinks } from '../data'
+import PageLinks from './PageLinks'
 
 const Footer = () => {
   return (
     <footer className="section footer">
-      <ul className="footer-links">
-        {pageLinks.map((link) => {
-          return (
-            <li key={link.id}>
-              <a href={link.href} className="footer-link">
-                {link.text}
-              </a>
-            </li>
-          )
-        })}
-      </ul>
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
       <ul className="footer-icons">
         {socialLinks.map((link) => {
           const { id, href, icon } = link
@@ -29,7 +20,7 @@ const Footer = () => {
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   )
