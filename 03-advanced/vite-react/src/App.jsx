e47ba584edb721670f464ef4d75ec8d461b14ef7
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2023-10-09 18:15:51 -0400

    fetchData

--- 03-advanced/vite-react/src/App.jsx
+++ 03-advanced/vite-react/src/App.jsx
@@ -1,8 +1,8 @@
-import UseEffectBasic from './JSX/UseEffectBasic'
+import FetchData from './JSX/FetchData'
 import './App.css'
 
 const App = () => {
-  return <UseEffectBasic />
+  return <FetchData />
 }
 
 export default App
--- 03-advanced/vite-react/src/JSX/UseEffectBasic.jsx
+++ 03-advanced/vite-react/src/JSX/UseEffectBasic.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 const UseEffectBasic = () => {
   const [value, setValue] = useState(0)
+  const [secondValue, setSecondValue] = useState(0)
   const sayHello = () => {
     console.log('hello there')
   }
@@ -9,12 +10,19 @@ const UseEffectBasic = () => {
   sayHello()
 
   useEffect(() => {
-    console.log('hello from useEffect')
-  })
+    console.log('hello from first useEffect')
+  }, [value])
+  useEffect(() => {
+    console.log('hello from second useEffect')
+  }, [secondValue])
   return (
     <div>
       <h1>value: {value}</h1>
       <button className="btn" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+      <h1>second value: {secondValue}</h1>
+      <button className="btn" onClick={() => setSecondValue(secondValue + 1)}>
         click me
       </button>
     </div>
